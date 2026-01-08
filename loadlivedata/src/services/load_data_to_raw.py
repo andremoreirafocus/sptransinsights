49b55315c4f298db6c10a92c5f18d4ee41d262af
andremoreirafocus <andre.moreira@focus-sys.com.br>
2026-01-08 15:52:52 -0300

    loadlivedata/refactor: remove comments and prints

--- loadlivedata/src/services/load_data_to_raw.py
+++ loadlivedata/src/services/load_data_to_raw.py
@@ -6,15 +6,10 @@ import json
 
 def load_data_to_raw(data, raw_bucket_name, app_folder, hour_minute):
     iso_timestamp_str = json.loads(data).get("metadata").get("extracted_at")
-    print(type(iso_timestamp_str))
     dt_object = datetime.fromisoformat(iso_timestamp_str)
-    print(f"Data extracted at: {dt_object}")
-    print(f"Loading data to raw bucket from {iso_timestamp_str}...")
-    # year = datetime.now().strftime("%Y")
     year = dt_object.year
     month = f"{dt_object.month:02d}"
     day = f"{dt_object.day:02d}"
-    print(f"Year: {year}, Month: {month}, Day: {day}")
     if data:
         prefix = f"{app_folder}/year={year}/month={month}/day={day}/"
         base_file_name = "posicoes_onibus"
