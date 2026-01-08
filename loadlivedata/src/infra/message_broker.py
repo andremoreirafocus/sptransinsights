56b3f6e04b6b6ffeb65d2ed11a9e1505de5528d2
andremoreirafocus <andre.moreira@focus-sys.com.br>
2026-01-08 12:22:28 -0300

    loadlivedata/feat: get summary of bus line data

---
+++ loadlivedata/src/infra/message_broker.py
@@ -0,0 +1,50 @@
+from kafka import KafkaConsumer
+import json
+
+
+def start_consumer(broker, topic):
+    num_read_messages = 0
+
+    print(f"[*] Connecting to {broker}...")
+
+    # Initialize the Consumer
+    consumer = KafkaConsumer(
+        topic,
+        bootstrap_servers=broker,
+        # Automatically handle JSON decoding
+        value_deserializer=lambda m: json.loads(m.decode("utf-8")),
+        # Start from the earliest messages if no offset is stored
+        auto_offset_reset="earliest",
+        # Group ID allows multiple consumers to work together
+        group_id="bus_monitor_group",
+    )
+
+    print(f"[*] Waiting for messages on topic: {topic}. To exit press CTRL+C")
+    print(f"Total messages read: {num_read_messages}\n")
+
+    try:
+        for message in consumer:
+            # 'message.value' is now a Python dictionary thanks to value_deserializer
+            payload = message.value
+            print(f"--- New Message Received at {message.timestamp} ---")
+
+            # If your payload is the 'teste' string or the bus dict:
+            data = json.loads(payload)
+
+            if isinstance(data, dict):
+                total_qv = 0
+                for line in data.get("l", []):
+                    # print(f"Line: {line.get('qv')}")
+                    total_qv += int(line.get("qv", 0))
+                print(
+                    f"Received data for {total_qv} vehicles from {len(data.get('l', []))} bus lines."
+                )
+            else:
+                print("Not a valid payload format.")
+                # print(f"Payload: {payload}")
+            num_read_messages += 1
+            print(f"Total messages read: {num_read_messages}\n")
+    except KeyboardInterrupt:
+        print("\nStopping consumer...")
+    finally:
+        consumer.close()
