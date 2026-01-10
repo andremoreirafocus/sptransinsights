ebb302cfe95ef91222fc34199cb55188f830dd53
andremoreirafocus <andre.moreira@focus-sys.com.br>
2026-01-10 07:42:13 -0300

    loadlivedata/fix: added broker message structure validation

--- loadlivedata/src/infra/message_broker.py
+++ loadlivedata/src/infra/message_broker.py
@@ -7,8 +7,46 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+def message_structure_is_valid(message):
+    """
+    Validate the structure of the incoming message.
+    :param message: The message to validate
+    :return: True if valid, False otherwise
+    """
+    if not isinstance(message, dict):
+        logger.error("Message does not have a valid structure.")
+        return False
+    required_fields = ["payload", "metadata"]
+    for field in required_fields:
+        if field not in message:
+            logger.error(f"Missing required field: {field}")
+            logger.error(f"Message content: {message}")
+            return False
+    if not isinstance(message.get("metadata"), dict):
+        logger.error("Message metadata does not have a valid structure.")
+        return False
+    required_fields = ["source", "extracted_at", "total_vehicles"]
+    for field in required_fields:
+        if field not in message.get("metadata"):
+            logger.error(f"Missing required metadata field: {field}")
+            logger.error(f"Metadata content: {message.get('metadata')}")
+            return False
+    if not isinstance(message.get("payload"), dict):
+        logger.error("Message payload does not have a valid structure.")
+        logger.error(f"Payload content: {message.get('payload')}")
+        logger.error(f"Metadata content: {message.get('metadata')}")
+        return False
+    required_fields = ["hr", "l"]
+    for field in required_fields:
+        if field not in message.get("payload"):
+            logger.error(f"Missing required payload field: {field}")
+            return False
+    return True
+
+
 def start_consumer(broker, topic, bucket_name, app_folder):
     num_read_messages = 0
+    num_invalid_messages = 0
 
     logger.info(f"[*] Connecting to {broker}...")
 
@@ -32,29 +70,29 @@ def start_consumer(broker, topic, bucket_name, app_folder):
             # 'message.value' is now a Python dictionary thanks to value_deserializer
             data_json = message.value
             logger.info(f"--- New Message Received at {message.timestamp} ---")
-
             data = json.loads(data_json)
-
-            if isinstance(data, dict):
-                total_qv = 0
-                for line in data.get("l", []):
-                    # logger.info(f"Line: {line.get('qv')}")
-                    total_qv += int(line.get("qv", 0))
-                logger.info(
-                    f"Received data for {total_qv} vehicles from {len(data.get('l', []))} bus lines."
-                )
-                load_data_to_raw(
-                    data=data_json,
-                    raw_bucket_name=bucket_name,
-                    app_folder=app_folder,
-                    hour_minute=data.get("payload")
-                    .get("hr")
-                    .replace(":", ""),  # e.g., "15:30" -> "1530"
-                )
-
-            else:
-                logger.error("Not a valid payload format.")
-                # logger.info(f"Payload: {payload}")
+            if not message_structure_is_valid(data):
+                num_invalid_messages += 1
+                logger.warning(f"Total invalid messages : {num_invalid_messages}\n")
+                return
+                continue
+            hour_minute = (
+                data.get("payload").get("hr").replace(":", ""),
+            )  # e.g., "15:30" -> "1530"
+            total_qv = 0
+            payload = data.get("payload")
+            for line in payload.get("l", []):
+                # logger.info(f"Line: {line.get('qv')}")
+                total_qv += int(line.get("qv", 0))
+            logger.info(
+                f"Received data for {total_qv} vehicles from {len(data.get('l', []))} bus lines."
+            )
+            load_data_to_raw(
+                data=data_json,
+                raw_bucket_name=bucket_name,
+                app_folder=app_folder,
+                hour_minute=hour_minute,
+            )
             num_read_messages += 1
             logger.info(f"Total messages read: {num_read_messages}\n")
     except KeyboardInterrupt:
