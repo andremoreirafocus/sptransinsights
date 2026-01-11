e7d74c45094fc06ad7990cc43b7f8aa7ccd0687f
andremoreirafocus <andre.moreira@focus-sys.com.br>
2026-01-11 14:59:26 -0300

    refactor: decouple db functions from transform function

--- transformlivedata/src/infra/db.py
+++ transformlivedata/src/infra/db.py
@@ -3,7 +3,15 @@ from contextlib import contextmanager
 
 import psycopg2
 from psycopg2.extras import DictCursor
+from psycopg2.extras import execute_values
+from psycopg2 import DatabaseError, InterfaceError
+
 from dotenv import load_dotenv
+import logging
+
+# This logger inherits the configuration from the root logger in main.py
+logger = logging.getLogger(__name__)
+
 
 # Load .env into environment variables
 load_dotenv()  # looks for a .env file in the current working directory by default.[web:9]
@@ -41,3 +49,39 @@ def db_cursor(commit: bool = True, cursor_factory=DictCursor):
         raise
     finally:
         conn.close()
+
+
+def bulk_insert_data_table(sql, data_table):
+    conn = None
+    try:
+        # 1. Initialize connection and cursor
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        # 2. Execute the batch insert
+        execute_values(cur, sql, data_table, page_size=1000)
+
+        # 3. Commit only if execution succeeds
+        conn.commit()
+        print(f"Successfully inserted {len(data_table)} rows into table")
+
+    except (DatabaseError, InterfaceError) as db_err:
+        # Rollback the transaction if any database error occurs
+        if conn:
+            conn.rollback()
+        logging.error(f"Database error during insert into table: {db_err}")
+        raise  # Re-raise so the orchestrator knows the pipeline failed
+
+    except Exception as e:
+        # Catch unexpected Python errors
+        if conn:
+            conn.rollback()
+        logging.error(f"Unexpected error during transformation: {e}")
+        raise
+
+    finally:
+        # 4. ALWAYS close the connection, regardless of success or failure
+        if conn:
+            cur.close()
+            conn.close()
+            print("Database connection closed.")
