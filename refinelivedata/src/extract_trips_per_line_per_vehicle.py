25b7bc8b704f523049c49d1ebf01dc231da5793f
andremoreirafocus <andre.moreira@focus-sys.com.br>
2026-01-19 14:46:27 -0300

    refinelivedata/feat: generate trips for a specific bus line and vehicle from positions and save to the db

--- refinelivedata/src/extract_trips_per_line_per_vehicle.py
+++ refinelivedata/src/extract_trips_per_line_per_vehicle.py
@@ -1,18 +1,38 @@
-import logging
-
 from src.services.load_positions_for_line_and_vehicle import (
     load_positions_for_line_and_vehicle,
 )
-from src.services.calculate_trips import calculate_trips
+from src.services.extract_trips_from_positions import (
+    extract_raw_trips_metadata,
+    generate_trips_table,
+)
 from src.services.save_trips_to_db import save_trips_to_db
+import logging
 
 # This logger inherits the configuration from the root logger in main.py
 logger = logging.getLogger(__name__)
 
 
 def extract_trips_per_line_per_vehicle(config, year, month, day, linha_lt, veiculo_id):
-    filtered_positions = load_positions_for_line_and_vehicle(
+    position_records = load_positions_for_line_and_vehicle(
         config, year, month, day, linha_lt, veiculo_id
     )
-    trips = calculate_trips(filtered_positions)
-    save_trips_to_db(config, trips)
+    if not position_records:
+        logger.error(
+            f"No positions retrieved for line {linha_lt} and vehicle {veiculo_id}"
+        )
+    logger.info("Extracting raw trips metadata from position records...")
+    raw_trips_metadata = extract_raw_trips_metadata(position_records)
+    logger.info(
+        f"Extracted {len(raw_trips_metadata)} raw trips metadata from position records for line {linha_lt} and vehicle {veiculo_id}."
+    )
+    # return raw_trips_metadata
+    clean_trips_metadata = raw_trips_metadata
+    logger.info("Generating records for trips...")
+    clean_trips = generate_trips_table(
+        position_records, clean_trips_metadata, linha_lt, veiculo_id
+    )
+    logger.info(f"Generated {len(clean_trips)} records for trips.")
+
+    logger.info(f"Saving {len(clean_trips)} trips to database...")
+    save_trips_to_db(config, clean_trips)
+    logger.info("Saved trips to database.")
--- refinelivedata/src/services/save_trips_to_db.py
+++ refinelivedata/src/services/save_trips_to_db.py
@@ -6,21 +6,16 @@ logger = logging.getLogger(__name__)
 
 
 def save_trips_to_db(config, trips_table):
-    """
-    Insert 10k+ items from memory list.
-    Assumes list format: (extracao_ts, veiculo_id, linha_lt, linha_code,
-                          linha_sentido, lt_destino, lt_origem, veiculo_prefixo,
-                          veiculo_acessivel, veiculo_ts, veiculo_lat, veiculo_long)
-    """
     table_name = config["FINISHED_TRIPS_TABLE_NAME"]
     insert_sql = f"""
     INSERT INTO {table_name} (
-        extracao_ts, veiculo_id, linha_lt, linha_code, linha_sentido,
-        lt_destino, lt_origem, veiculo_prefixo, veiculo_acessivel, veiculo_ts,
-        veiculo_lat, veiculo_long, is_circular, first_stop_id, first_stop_lat,
-        first_stop_lon, last_stop_id, last_stop_lat, last_stop_lon,
-        distance_to_first_stop, distance_to_last_stop
+            trip_id,
+            vehicle_id,
+            trip_start_time,
+            trip_end_time,
+            duration,
+            is_circular,
+            average_speed
     ) VALUES %s
     """
-
     bulk_insert_data_table(config, insert_sql, trips_table)
