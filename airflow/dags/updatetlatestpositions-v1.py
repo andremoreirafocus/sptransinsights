127eb9fda8bdea8fe84b2be65d51a58e3dce95eb
andremoreirafocus <andre.moreira@focus-sys.com.br>
2026-01-20 09:16:25 -0300

    airflow/feat: added updatelatestposition DAG

---
+++ airflow/dags/updatetlatestpositions-v1.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from updatelatestpositions.services.create_latest_positions import (
+    create_latest_positions_table,
+)
+from updatelatestpositions.config import get_config
+
+# Definindo os argumentos padrão para as tarefas do DAG
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": days_ago(0),
+    "max_active_runs": 1,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 3,
+}
+
+
+def update_latest_positions_table():
+    config = get_config()
+    create_latest_positions_table(config)
+
+
+# Criando o DAG
+with DAG(
+    "updatelatestposition-v1",
+    default_args=default_args,
+    description="Load latest positions to refined.latest_positions table",
+    schedule_interval="*/2 * * * *",  # Use cron expression for every minute
+    catchup=False,
+) as dag:
+    update_latest_positions_task = PythonOperator(
+        task_id="update_to_db",
+        python_callable=update_latest_positions_table,
+    )
+
+    update_latest_positions_task
